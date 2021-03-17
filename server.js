d17db082b0e93f1fa9604e901c740a8db14f3dd3
Данил <abanin-02@mail.ru>
2021-03-17 19:50:11 +0400

    Build Heroku #2

--- server.js
+++ server.js
@@ -1,11 +1,9 @@
 const express = require('express');
 const path = require('path');
-const favicon = require('express-favicon');
 const port = process.env.PORT || 3000;
 
 // здесь у нас происходит импорт пакетов и определяется порт нашего сервера
 const app = express();
-app.use(favicon(__dirname + '/build/favicon.png'));
 //здесь наше приложение отдаёт статику
 app.use(express.static(__dirname));
 app.use(express.static(path.join(__dirname, 'build')));
